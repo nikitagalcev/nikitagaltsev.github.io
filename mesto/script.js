95f4585b01f73273773ac004fb72ff6a8d57835d
nikitagaltsev <nikitagalcev@gmail.com>
2020-05-08 22:32:23 +0300

    Iter 2

--- mesto/script.js
+++ mesto/script.js
@@ -43,21 +43,43 @@ const initialCards = [
 
 // Декларация переменных
 const placesContainer = document.querySelector('.places-list');
-const form = document.forms.new;
-const inputName = form.elements.name;
-const inputLink = form.elements.link;
-const inputButton = document.querySelector('.popup__button');
-inputName.setAttribute('required', true);
-inputLink.setAttribute('required', true);
+
+const formPlace = document.forms.place;
+const formPerson = document.forms.profile;
+
+const inputPlaceName = formPlace.elements.name;
+const inputPlaceLink = formPlace.elements.link;
+const inputPersonName = formPerson.elements.name;
+const inputPersonAbout = formPerson.elements.about;
+
+const popupPerson = document.querySelector('.popup_person');
+const popupPlace = document.querySelector('.popup_place');
+const popupImage = document.querySelector('.popup_image');
+
+const userInfoName = document.querySelector('.user-info__name');
+const userInfoJob = document.querySelector('.user-info__job');
+const personButton = document.querySelector('.popup__button_person');
+const placeButton = document.querySelector('.popup__button_place');
+
+const errorMessages = {
+  empty: 'Это обязательное поле',
+  tooShort: 'Должно быть от 2 до 30 символов',
+  url: 'Здесь должна быть ссылка'
+};
 
 const customCard = {
   name: '',
   link: ''
 };
 
+const personInfo = {
+  name: '',
+  about: ''
+};
+
 
 // Функции
-//Создание карточки
+// Создание карточки
 const createPlaceCard = function() {
   const markup = `
     <div class="place-card">
@@ -73,21 +95,21 @@ const createPlaceCard = function() {
 
   const placeCard = document.createElement('div');
   placeCard.insertAdjacentHTML('afterbegin', markup);
-
+  /*REVIEW. Отлично, что функция createPlaceCard отвечает только за создание шаблона карточки, а добавление карточки к общему списку
+  происходит в другой функции. Это соответствует принципу единственной ответственности функции. Такие функции, независящие от размётки
+  страницы, можно переиспользовать в других проектах. */
   return placeCard.firstElementChild;
 };
 
 
-
-// Удаление карточки
-const removeCardHandler = function(event) {
-  event.target.closest('.place-card').remove();
-};
-
+// popup для изображений
+const imagePopup = function(event) {
+  const backgroundImage = event.target.style.backgroundImage.split('').slice(5, length-2).join('');
+  document.querySelector('.popup__image-open').setAttribute('src', backgroundImage);
+  showPopup(popupImage);
+}
 
 // Отрисовка карточки и определение её элементов
-
-
 const renderPlaceCard = function(item) {
   const newPlaceCard = createPlaceCard();
   newPlaceCard.querySelector('.place-card__image').setAttribute('style', `background-image: url(${item['link']})`);
@@ -96,46 +118,197 @@ const renderPlaceCard = function(item) {
   const likeButton = newPlaceCard.querySelector('.place-card__like-icon');
   const deleteButton = newPlaceCard.querySelector('.place-card__delete-icon');
 
-  deleteButton.addEventListener('click', function(event) {
-    removeCardHandler(event);
-  });
+  /*REVIEW. Можно лучше. Обработчиками событий карточки лучше делать именованные функции, а не безымянные, чтобы эти обработчики можно было удалять при удалении самой карточки - это будет обязательным в 8-м задании.*/
+  
+  // Исправил, также вынес функции из тела renderPlaceCard 
 
-  likeButton.addEventListener('click', function(event) {
-    event.target.classList.toggle('place-card__like-icon_liked');
-  })
+  deleteButton.addEventListener('click', deleteCard);
+  likeButton.addEventListener('click', likeCard);
+  newPlaceCard.querySelector('.place-card__image').addEventListener('click', imagePopup);
 
   placesContainer.appendChild(newPlaceCard);
 };
 
-// Управление отображением формы
-const showForm = function() {
-  document.querySelector('.popup').classList.toggle('popup_is-opened');
+function deleteCard() {
+  event.target.closest('.place-card').remove();
+  event.stopPropagation();
+};
+
+function likeCard() {
+  event.target.classList.toggle('place-card__like-icon_liked');
 };
 
 
-//Добавление кастомой карточки
+// Добавление кастомной карточки
 const userPlaceCard = function() {
-  customCard.name = inputName.value;
-  customCard.link = inputLink.value;
+  customCard.name = inputPlaceName.value;
+  customCard.link = inputPlaceLink.value;
   initialCards.push(customCard);
-  form.reset();
+  formPlace.reset();
   renderPlaceCard(initialCards[initialCards.length-1]);
+};
+
+// Инициализация пользователя
+const changeUserInfo = function() {
+  document.querySelector('.popup__input_type_person-name').setAttribute('value', userInfoName.textContent);
+  document.querySelector('.popup__input_type_about').setAttribute('value', userInfoJob.textContent);
+};
+
+// Изменение пользователя
+const person = function() {
+  personInfo.name = inputPersonName.value;
+  personInfo.about = inputPersonAbout.value;
+  formPerson.reset();
+  userInfoName.textContent = personInfo.name;
+  userInfoJob.textContent = personInfo.about;
+  changeUserInfo();
+};
+
+// Отображение попапа
+
+const showPopup = function(popup) {
+  /*REVIEW. Надо исправить. Нельзя обращаться к DOM-элементам по индексам. Это сильно осложняет сопровождение и расширении проекта. При добавлении
+  ещё одного всплывающего окна формы (а это будет в 9-м задании), Вы вряд ли избежите проверки существующего кода, которая будет нужна
+  чтобы избежать конфликты номеров всплывающих окон. А по принципу открытости-закрытости, которому должны подчиняться все проекты, написанные на js,
+  расширение проекта никак не должно затрагивать старый код. Поэтому к DOM-элементам надо обращаться по индивидуальным селекторам, отражающим их суть,
+  а не номер, и этот селектор, в соответствии с системой БЭМ, должен быть классом, а не идентификатором. Индексы можно использовать только при переборе
+  коллекции элементов в цикле.
+  Устраните использование индексов DOM-элементов во всём проекте. */
+
+  //Исправил на названия классов, но, знаю что (вроде бы) можно делать через bind. Эти темы будут в следующих спринтах, потому пока боюсь за это браться.
+
+  popup.classList.toggle('popup_is-opened');
+  popup.querySelector('.popup__close').addEventListener('click', e => {
+    popup.classList.remove('popup_is-opened');
+  });
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      popup.classList.remove('popup_is-opened');
+    };
+  });
+};
+
+
+// Валидация, любимая
+
+// Управление кнопками
+const blockButton = function(button, status) {
+  if (status === true) {
+    button.classList.remove('popup__button_active');
+    button.disabled = true;
+    button.style.cursor = 'not-allowed';
+  } else {
+    button.classList.add('popup__button_active');
+    button.disabled = false;
+    button.style.cursor = 'pointer';
+  };
+};
+
+//Сбросить все ошибки на всех формах
+function resetErrors() {
+  document.querySelectorAll('.error-message').forEach(item => {
+    item.textContent = '';
+  });
+};
+
+//Обнулить поля и заблочить кнопку в place форме
+function resetPlaceFields() {
+  inputPlaceName.value = '';
+  inputPlaceLink.value = '';
+  blockButton(placeButton, true);
+};
+
+// Для person
+function actualizePersonFields() {
+  inputPersonName.value = userInfoName.textContent;
+  inputPersonAbout.value = userInfoJob.textContent;
+  blockButton(personButton, false);
+  resetErrors();
+};
+
+const personValidity = function() {
+  const currentInput = event.target;
+  const errorELem = currentInput.parentNode.querySelector(`#error-${currentInput.id}`);
+  if(currentInput.value.length === 0 || currentInput.value.length === 0) {
+    currentInput.setCustomValidity(errorMessages.empty);
+    blockButton(personButton, true);
+  } else if(currentInput.value.length < 2 || currentInput.value.length > 30) {
+    currentInput.setCustomValidity(errorMessages.tooShort);
+    blockButton(personButton, true);
+  } else {
+    currentInput.setCustomValidity('');
+    blockButton(personButton, false);
+  };
+  errorELem.textContent = currentInput.validationMessage;
+};
+
+// Для place
+const placeValidity = function() {
+  if (inputPlaceName.value.length === 0 || inputPlaceLink.value.length === 0) {
+    blockButton(placeButton, true);
+  } else {
+    blockButton(placeButton, false);
+  };
 };
 
 
 // Слушатели
-document.querySelector('.user-info__button').addEventListener('click', showForm);
-document.querySelector('.popup__close').addEventListener('click', showForm);
+formPerson.addEventListener('input', personValidity);
+formPlace.addEventListener('input', placeValidity);
 
+
+document.querySelector('.user-info__edit-button').addEventListener('click', e => { showPopup(popupPerson); actualizePersonFields(); });
+document.querySelector('.user-info__button').addEventListener('click', e => { showPopup(popupPlace); resetPlaceFields(); });
+
+formPerson.addEventListener('submit', function(event) {
+  event.preventDefault();
+  showPopup(popupPerson);
+  person();
+});
+
+formPlace.addEventListener('submit', function(event) {
+  userPlaceCard();
+  event.preventDefault();
+  showPopup(popupPlace);
+});
 
 // Вызовы функций и методов
+changeUserInfo();
+
 initialCards.forEach(function(item) {
   renderPlaceCard(item);
 });
 
 
-form.addEventListener('submit', function(event) {
-  userPlaceCard();
-  event.preventDefault();
-  showForm();
-})
+
+/*REVIEW. Резюме.
+
+Работа неплохая. Функционал, требуемый по заданию, работает, кроме до конца правильной валидации обеих форм.
+
+Так же требуется оптимизация проекта.
+
+Что надо исправить.
+
+1. Устранить использование индексов DOM-элементов во всём проекте (подробности в ревью в коде showPopup). --Исправил
+
+2. По чеклисту требуется, чтобы на формах не появлялись системные сообщения об ошибках. Сейчас они появляются на форме карточки.
+Сделайте минимальную валидацию формы добавления новой карточки. По заданию даже не требуется полной валидации этой формы, достаточно
+только сделать так, чтобы кнопка сабмита этой формы была заблокирована, если хотя бы одно из полей форм пустое, и была разблокирована,
+если в обоих полях есть какая-то информация, то есть оба поля непустые. Сообщения об ошибках при минимальной валидации высвечиваться
+не должны.
+
+--Сделал минимально, пытался сделать полную валидацию, так и не вышло :(
+
+
+
+
+3. Нужно до конца правильной сделать валидацию формы профиля. Сейчас она работает правильно только наполовину, только когда Вы выходите
+из формы по сабмиту, но, если выйти из формы по крестику, предварительно сделав информацию в полях невалидной, то при повторном входе в
+форму информация со страницы на неё не переносится, хотя по заданию эта информация должна переноситься на форму в любом случае при её открытии.
+На форме видны сообщения об ошибках, оставшиеся от предыдущего неправильного ввода, чего быть не должно, так как на форме при её открытии всегда должна находиться валидная информация. Поэтому в слушателе открытия формы профиля, нужно переносить информацию со страницы в поля формы, производить удаление сообщений об ошибках, делать кнопку сабмита активной и чёрного цвета. Это можно будет сделать с помощью вызовов функции валидации полей формы профиля в слушателе события открытия формы. --Исправил
+
+Что можно улучшить.
+
+1. Обработчиками событий карточки лучше делать именованные функции (подробности в ревью в коде renderPlaceCard). --Исправил
+
+*/
